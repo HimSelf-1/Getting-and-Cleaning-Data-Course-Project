316272983aa0cb9cea8f13e0ed61635caa7930e8
HimSelf-1 <david_odeens@att.net>
2017-02-11 12:59:04 -0500

    Add files via upload
    Add files for project

--- README.md
+++ README.md
@@ -1,2 +1,112 @@
 # Getting-and-Cleaning-Data-Course-Project
 Getting and Cleaning Data Course Project
+Peer-graded Assignment: Getting and Cleaning Data Course Project
+Analysis files:
+==================================================================
+Experiments have been carried out with a group of 30 volunteers. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone on their waist. Using its embedded accelerometer and gyroscope, the study captured 3-axial linear acceleration and 3-axial angular velocity. The obtained dataset was randomly partitioned into two sets, where 70% of the volunteers was selected for generating the training data and 30% the test data. 
+The sensor signals (accelerometer and gyroscope) were pre-processed to obtain 128 readings/window.. From each window, a vector of features was obtained by calculating variables from the time and frequency domain. See the study “Human Activity Recognition Using Smartphones Dataset“ 'features_info.txt' for more details.  http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+For each record the study provided:
+=====================================
+- Triaxial acceleration from the accelerometer (total acceleration) and the estimated body acceleration.
+- Triaxial Angular velocity from the gyroscope. 
+- A 561-feature vector with time and frequency domain variables. 
+- Its activity label. 
+- An identifier of the subject who carried out the experiment.
+The dataset analized includes the following files:
+=========================================
+- 'README.txt'
+- 'features_info.txt': Shows information about the variables used on the feature vector.
+- 'features.txt': List of all features.
+- 'activity_labels.txt': Links the class labels with their activity name.
+- 'train/X_train.txt': Training set.
+- 'train/y_train.txt': Training labels.
+- 'test/X_test.txt': Test set.
+- 'test/y_test.txt': Test labels.
+The following files are available for the train and test data. Their descriptions are equivalent. 
+- 'train/subject_train.txt': Each row identifies the subject who performed the activity for each window sample. Its range is from 1 to 30. 
+- 'train/Inertial Signals/total_acc_x_train.txt': The acceleration signal from the smartphone accelerometer X axis in standard gravity units 'g'. Every row shows a 128 element vector. The same description applies for the 'total_acc_x_train.txt' and 'total_acc_z_train.txt' files for the Y and Z axis. 
+- 'train/Inertial Signals/body_acc_x_train.txt': The body acceleration signal obtained by subtracting the gravity from the total acceleration. 
+- 'train/Inertial Signals/body_gyro_x_train.txt': The angular velocity vector measured by the gyroscope for each window sample. The units are radians/second. 
+
+License:
+========
+Use of this dataset in publications must be acknowledged by referencing the following publication [1] 
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+
+
+Explanation of how the script works:   One R script run_analysis.R was created that does the following.
+1.	Merges the training and the test sets to create one data set.
+2.	Extracts only the measurements on the mean and standard deviation for each measurement.
+3.	Uses descriptive activity names to name the activities in the data set
+4.	Appropriately labels the data set with descriptive variable names.
+5.	From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+Detail of steps:
+Step 1.       Merge the training and the test sets to create one data set.
+	1.	Create data directory if it does not exist
+	2.	Download data: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+	3.	Read in the data into tables - NOTE:  the suffix _train & _test refer to the group the subjects were placed in
+	•	Subject files using subject_train.txt & subject_test.txt which contains the subject identification
+		o	subject_train
+		o	subject_test
+	•	Activity files using Y_train.txt & Y_test.txt which contains the activity the subject performed
+		o	activity_train
+		o	activity_test
+	•	Feature files using X_train.txt & X_test.txt which contains the data from the activities the subjects performed
+		o	feature_train
+		o	feature_test
+ 	4. Merge the training and the test sets to create one data set
+	•	Merge like data into a single file
+		o	subject_df using files subject_train & subject_test
+		o	activity_df using files activity_train & activity_test
+		o	features_df using files feature_train & feature_test
+ 	5. Assign names to variables from provided data set
+	•	subject_df  assign subject
+	•	activity_df assign activity
+ 	6. Read descriptive feature names from features.txt into featureNames
+ 	* Rename variables in features_df using the feature names from column 2 of featureNames
+ 	7. Merge columns to get a data frame for all data
+		1.	data_s_a using files subject_df & activity_df
+		2.	total_data using files features_df & data_s_a
+Step 2.	Extract only the measurements on the mean and standard deviation for each measurement.
+	1.	filter_on_names_of_int from file featureNames with contain strings with mean() or std() in name of features
+	2.	Subset into names_of_int based on subject & activity from filter_on_names_of_int
+	•	data_of_int is a subset of total_data based on names_of_int
+Step 3.	Use descriptive activity names to name the activities in the data set
+ 	1. Read descriptive activity names from activity_labels.txt into activityNames
+ 	2. Factor variable activity in data_of_int
+ 	3. Overwrite activity in data_of_int with descriptive activity names
+Step 4.	Label the data set with descriptive variable names.
+	Already completed in the R Script:    
+	•	Re-labeled activity in data_of_int with descriptive activity name
+	•	Read descriptive feature names from features.txt
+	•	Assigned names to variables from provided data set : subject & activity
+	Based on the features_info.txt file, the following actions were taken:
+	•	Time domain signals are prefixed with a 't'; strings beginning with a 't' replaced with 'time'
+	•	Frequency domain signals are prefixed with an 'f'; strings beginning with an 'f' replaced with 'frequency'
+	•	Strings containing 'Acc' were replaced with 'Accelerometer'
+	•	Strings containing 'Gyro' were replaced with 'Gyroscope'
+	•	Strings containing 'Mag' were replaced with 'Magnitude'
+	•	Strings containing 'BodyBody' were replaced with 'Body_Force_Calculated'
+	•	Strings containing 'mean()' were replaced with 'Mean_value'
+	•	Strings containing 'std()' were replaced with 'Standard_deviation'
+Step 5.	From the data set in step 4, create a second, independent tidy data set with the average of each variable for each activity and each subject.
+	•	Load library 'plyr'
+	1.	Aggregate data frame data_of_int by each activity and subjectinto df, returning means for numeric variables
+	2.	Order/sort the results in df by subject and then activity
+	3.	Save  the tidy data df into tidydataset.txt
+Data tables of analysis observations:
+	data table data_of_int with all variables along with units
+		•	contains 10,299 obs. of 68 variables
+		•	subject and activity variables are interger values
+		•	subject is an unique id for each member of the group of 30 volunteers
+		•	Each subject performed six activities : WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING
+		•	Variables prefixed with time have units of seconds, and those with frequency are in Hertz (Hz)
+		•	all variables prefixed with time or frequency are numeric measurement values from the experiment:
+	data table df with all variables and summaries calculated mean along with units
+		•	contains 180 obs. of 68 variables
+		•	subject and activity variables are interger values
+		•	subject is an unique id for each member of the group of 30 volunteers
+		•	Each subject performed six activities : WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING
+		•	Variables prefixed with time have units of seconds, and those with frequency are in Hertz (Hz)
+		•	all variables prefixed with time or frequency are the numeric mean of the experiment measurements.
---
+++ run_analysis.R
@@ -0,0 +1,120 @@
+## run_analysis.R
+## rm(list=ls())  # clean the workspace  
+setwd("C:/Users/Dave/Getting_and_Cleaning_Data_Course_Project")
+if(!file.exists("./accelerometer")) {dir.create("./accelerometer")}  ## create data directory if it does not exist
+dataset_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(dataset_url, destfile = "./accelerometer/accelerometer.zip")  ## save in working directory
+unzip(zipfile = "./accelerometer/accelerometer.zip", exdir = "./accelerometer")  ## unzip the file into your R directory
+path_read_files <- file.path("./accelerometer","UCI HAR Dataset")
+files <- list.files(path_read_files, recursive = TRUE)  ## get a list of the files in 'UCI HAR Dataset' folder
+files    
+## read in the data into tables : subject files
+subject_train <- read.table(file.path(path_read_files,"train","subject_train.txt"), header = FALSE) 
+subject_test <- read.table(file.path(path_read_files,"test","subject_test.txt"), header = FALSE)  
+## read in the data into tables : Activity files
+activity_train <- read.table(file.path(path_read_files,"train","Y_train.txt"), header = FALSE)
+activity_test <- read.table(file.path(path_read_files,"test","Y_test.txt"), header = FALSE)
+## read in the data into tables : Feature
+feature_train <- read.table(file.path(path_read_files,"train","X_train.txt"), header = FALSE)
+feature_test <- read.table(file.path(path_read_files,"test","X_test.txt"), header = FALSE)
+table(subject_test);table(subject_train)
+## subject_test  ## contains an identifier of the subject who carried out the experiment and the number of experiments involved in
+##   2   4   9  10  12  13  18  20  24 
+## 302 317 288 294 320 327 364 354 381 
+## subject_train  ## contains an identifier of the subject who carried out the experiment and the number of experiments involved in
+##   1   3   5   6   7   8  11  14  15  16  17  19  21  22  23  25  26  27  28  29  30 
+## 347 341 302 325 308 281 316 323 328 366 368 360 408 321 372 409 392 376 382 344 383 
+table(activity_test);table(activity_train)
+## activity_test
+##   1   2   3   4   5   6 
+## 496 471 420 491 532 537 
+## activity_train
+##    1    2    3    4    5    6 
+## 1226 1073  986 1286 1374 1407 
+str(feature_test)
+str(feature_train)
+## 1.	Merge the training and the test sets to create one data set
+        ## merge like data into a single file
+subject_df <- rbind(subject_train,subject_test)
+activity_df <- rbind(activity_train,activity_test)
+features_df <- rbind(feature_train,feature_test)
+        ## assign names to variables from provided data set
+names(subject_df) <- c("subject")
+names(activity_df) <- c("activity")
+featureNames <- read.table(file.path(path_read_files,"features.txt"),head = FALSE) ## read descriptive feature names from "features.txt"
+names(features_df) <- featureNames$V2  ## use column 2 names to rename variables
+features_df
+        ## merge columns to get data frame for all data
+data_s_a <- cbind(subject_df,activity_df)  ## step one, merge two into one
+total_data <- cbind(features_df,data_s_a)  ## step two, merge the last file with other data
+
+
+##  2.	Extract only the measurements on the mean and standard deviation for each measurement
+filter_on_names_of_int <- featureNames$V2[grep("mean\\(\\)|std\\(\\)",featureNames$V2)]  ## locate strings with mean() or std()contained in name of features
+## attributes(filter_on_names_of_int)
+names_of_int <- c(as.character(filter_on_names_of_int), "subject", "activity")  ## subset based on names of interest
+data_of_int <- subset(total_data, select = names_of_int)
+str(data_of_int)
+head(data_of_int$activity,30)
+## 3.	Uses descriptive activity names to name the activities in the data set
+activityNames <- read.table(file.path(path_read_files,"activity_labels.txt"),head = FALSE) ## read descriptive activity names from "activity_labels.txt"
+names(activityNames)
+## [1] "V1" "V2"
+data_of_int$activity <- factor(data_of_int$activity)  ## factor variable 'activity' in 'data_of_int'
+data_of_int$activity <- factor(data_of_int$activity,labels=as.character(activityNames$V2))  ## overwrite 'activity' in 'data_of_int' with descriptive activity names
+head(data_of_int$activity,28)  ## check ouput  -- record 28 should be SITTING - output is validated
+
+## 4.	Appropriately labels the data set with descriptive variable names
+        ## already in this R Script:
+                ## re-labeled 'activity' in 'data_of_int' with descriptive activity name
+                ## and read descriptive feature names from "features.txt"
+                ## and assigned names to variables from provided data set :"subject" & "activity"
+names(data_of_int)  ## check column names to see what current state is with goal of what can be done to make variable names more descriptive
+##  [1] "tBodyAcc-mean()-X"           "tBodyAcc-mean()-Y"           "tBodyAcc-mean()-Z"           "tBodyAcc-std()-X"           
+##  [5] "tBodyAcc-std()-Y"            "tBodyAcc-std()-Z"            "tGravityAcc-mean()-X"        "tGravityAcc-mean()-Y"       
+##  [9] "tGravityAcc-mean()-Z"        "tGravityAcc-std()-X"         "tGravityAcc-std()-Y"         "tGravityAcc-std()-Z"        
+## [13] "tBodyAccJerk-mean()-X"       "tBodyAccJerk-mean()-Y"       "tBodyAccJerk-mean()-Z"       "tBodyAccJerk-std()-X"       
+## [17] "tBodyAccJerk-std()-Y"        "tBodyAccJerk-std()-Z"        "tBodyGyro-mean()-X"          "tBodyGyro-mean()-Y"         
+## [21] "tBodyGyro-mean()-Z"          "tBodyGyro-std()-X"           "tBodyGyro-std()-Y"           "tBodyGyro-std()-Z"          
+## [25] "tBodyGyroJerk-mean()-X"      "tBodyGyroJerk-mean()-Y"      "tBodyGyroJerk-mean()-Z"      "tBodyGyroJerk-std()-X"      
+## [29] "tBodyGyroJerk-std()-Y"       "tBodyGyroJerk-std()-Z"       "tBodyAccMag-mean()"          "tBodyAccMag-std()"          
+## [33] "tGravityAccMag-mean()"       "tGravityAccMag-std()"        "tBodyAccJerkMag-mean()"      "tBodyAccJerkMag-std()"      
+## [37] "tBodyGyroMag-mean()"         "tBodyGyroMag-std()"          "tBodyGyroJerkMag-mean()"     "tBodyGyroJerkMag-std()"     
+## [41] "fBodyAcc-mean()-X"           "fBodyAcc-mean()-Y"           "fBodyAcc-mean()-Z"           "fBodyAcc-std()-X"           
+## [45] "fBodyAcc-std()-Y"            "fBodyAcc-std()-Z"            "fBodyAccJerk-mean()-X"       "fBodyAccJerk-mean()-Y"      
+## [49] "fBodyAccJerk-mean()-Z"       "fBodyAccJerk-std()-X"        "fBodyAccJerk-std()-Y"        "fBodyAccJerk-std()-Z"       
+## [53] "fBodyGyro-mean()-X"          "fBodyGyro-mean()-Y"          "fBodyGyro-mean()-Z"          "fBodyGyro-std()-X"          
+## [57] "fBodyGyro-std()-Y"           "fBodyGyro-std()-Z"           "fBodyAccMag-mean()"          "fBodyAccMag-std()"          
+## [61] "fBodyBodyAccJerkMag-mean()"  "fBodyBodyAccJerkMag-std()"   "fBodyBodyGyroMag-mean()"     "fBodyBodyGyroMag-std()"     
+## [65] "fBodyBodyGyroJerkMag-mean()" "fBodyBodyGyroJerkMag-std()"  "subject"                     "activity" 
+
+## from 'features_info.txt' file:
+        ## time domain signals (prefix 't' to denote time - prefix
+names(data_of_int)<-gsub("^t", "time", names(data_of_int))  ## metacharacter ^ represents start of line
+        ## Note the 'f' to indicate frequency domain signals
+names(data_of_int)<-gsub("^f", "frequency", names(data_of_int))  ## metacharacter ^ represents start of line
+        ## Acc is short for accelerometer
+names(data_of_int)<-gsub("Acc", "Accelerometer", names(data_of_int))
+        ## Gyro is short for gyroscope
+names(data_of_int)<-gsub("Gyro", "Gyroscope", names(data_of_int))
+        ## Mag is short for magnitude
+names(data_of_int)<-gsub("Mag", "Magnitude", names(data_of_int))
+        ## BodyBody can be made to body_force_calculated to be more descriptive
+names(data_of_int)<-gsub("BodyBody", "Body_Force_Calculated", names(data_of_int))
+        ## mean(): Mean value
+names(data_of_int)<-gsub("mean\\(\\)", "Mean_value", names(data_of_int))  ## \\ used to escape special characters
+        ## std(): Standard deviation
+names(data_of_int)<-gsub("std\\(\\)", "Standard_deviation", names(data_of_int)) ## \\ used to escape special characters
+## names(data_of_int)
+
+
+
+## 5.	From the data set in step 4, creates a 
+##      second, independent tidy data set with the average of each variable for each activity and each subject.
+
+## install.packages("plyr")  ## install package 'plyr'
+library(plyr)   ## load package 'plyr'
+# aggregate data frame data_of_int by each activity and subject, returning means for numeric variables
+df <-aggregate(. ~subject + activity, data_of_int, mean)
+df<-df[order(df$subject,df$activity),]   ## order/sort the results by subject and then activity
+write.table(df, file = "tidydataset.txt",row.name=FALSE)
